29bad7d3ba8d36f7509de1201ef5885e18dc843b
Akhil <akhilprasad94@gmail.com>
2020-06-25 00:47:50 -0400

    Security changes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,37 @@ import UpdateProject from "./components/Project/UpdateProject";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
 import AddProjectTask from "./components/ProjectBoard/ProjectTask/AddProjectTask";
 import UpdateProjectTask from "./components/ProjectBoard/ProjectTask/UpdateProjectTask";
+import Landing from "./components/Layout/Landing";
+import Register from "./components/UserManagement/Register";
+import Login from "./components/UserManagement/Login";
+import jwt_decode from "jwt-decode";
+import setJWTToken from "./securityUtils/setJWTToken";
+import { SET_CURRENT_USER } from "./actions/types";
+import {logout} from "./actions/securityActions";
+
+
+
+const jwtToken = localStorage.jwtToken;
+
+if(jwtToken) {
+  setJWTToken(jwtToken)
+  const decoded_jwtToken = jwt_decode(jwtToken);
+  store.dispatch(
+    {
+      type: SET_CURRENT_USER,
+      payload: decoded_jwtToken
+    }
+  );
+
+  const currentTime = Date.now()/1000
+  if(decoded_jwtToken.exp < currentTime){
+      store.dispatch(logout());
+      window.location.href = "/";
+  }
+}
+
+
+
 class App extends Component {
   render() {
     return (
@@ -18,6 +49,15 @@ class App extends Component {
         <Router>
           <div className="App">
             <Header />
+            {
+              //Public routes
+            }
+            <Route exact path="/" component = {Landing} />
+            <Route exact path="/register" component = {Register} />
+            <Route exact path="/login" component = {Login} />
+            {
+              //Private routes
+            }
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
             <Route exact path="/updateProject/:id" component={UpdateProject} />
